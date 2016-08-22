5a0ad11d76db45176fffb81733bc5d7ebfb09ab0
sunurdy <sunurdy@gmail.com>
2016-08-22 21:15:12 +0800

    获取用户的所有订单

---
+++ handler/order/all_handler.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@version: 
+@author:
+@time: 2016/8/22 16:29
+"""
+
+import json
+from model.models import db, Order, User, Purchase
+from handler.base.base_handler import BaseHandler
+
+
+class AllHandler(BaseHandler):
+    def get(self):
+        res = []
+        open_id = self.get_argument('open_id', None)
+        if open_id:
+            user = db.query(User).filter_by(open_id=open_id).first()
+            if user:
+                all_orders = db.query(Order).filter_by(user_id=user.id).all()
+                for i in all_orders:
+                    res.append(i.items)
+
+        # 返回数据，如果reason为空则表示成功，否则表示出错
+        self.write({'reason': '', 'res': res})
