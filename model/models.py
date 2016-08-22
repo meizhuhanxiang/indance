9281d657a1dec10af709ee66ff7bd0017a435f60
sunurdy <sunurdy@gmail.com>
2016-08-22 17:18:41 +0800

    change model

--- model/models.py
+++ model/models.py
@@ -1,90 +1,51 @@
 # coding: utf-8
-
-
+from sqlalchemy import Column, DateTime, Integer, String, text
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, DateTime, Integer, String, text, create_engine
+
 
 Base = declarative_base()
 metadata = Base.metadata
 
 
-class Addres(Base):
-    __tablename__ = 'address'
+class Kind(Base):
+    __tablename__ = 'kind'
 
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer)
-    name = Column(Integer)
-    country = Column(Integer)
-    province = Column(Integer)
-    reginon = Column(Integer)
-    address = Column(String(50))
-    phone = Column(Integer)
-    default = Column(Integer, server_default=text("'0'"))
-    update_time = Column(DateTime)
-
-
-class Cart(Base):
-    __tablename__ = 'cart'
-
-    id = Column(Integer, primary_key=True)
-    seed_id = Column(Integer, nullable=False)
-    properties = Column(String(100), nullable=False, server_default=text("''"))
-    count = Column(Integer, nullable=False)
-    make_order = Column(Integer, nullable=False, server_default=text("'0'"))
-    update_time = Column(DateTime, nullable=False)
+    purchase_id = Column(Integer)
+    kind = Column(String(11))
 
 
 class Order(Base):
     __tablename__ = 'order'
 
     id = Column(Integer, primary_key=True)
-    seed_id = Column(Integer, nullable=False)
-    address_id = Column(Integer, nullable=False)
-    status = Column(Integer, nullable=False)
-    update_time = Column(DateTime, nullable=False)
+    status = Column(String(11))
+    user_id = Column(String(11))
+    kind_id = Column(String(11))
+    pay_time = Column(Integer)
+    verify_time = Column(Integer)
+    verify_count = Column(Integer)
 
 
 class Publisher(Base):
     __tablename__ = 'publisher'
 
     id = Column(Integer, primary_key=True)
-    logo = Column(String(30))
-    navigation_img = Column(String(30), nullable=False, server_default=text("''"))
-    brief_introduction = Column(String(60), nullable=False, server_default=text("''"))
-    activity_photos = Column(String(30), nullable=False, server_default=text("''"))
-    admin_name = Column(String(16), nullable=False, server_default=text("''"))
-    admin_phone = Column(Integer, nullable=False)
-    admin_id_card_number = Column(String(20), nullable=False, server_default=text("''"))
-    admin_id_card_img = Column(String(30), nullable=False, server_default=text("''"))
-    other_title = Column(String(20), nullable=False, server_default=text("''"))
-    other_content = Column(String(60), nullable=False, server_default=text("''"))
-    update_time = Column(DateTime, nullable=False)
+    name = Column(String(30))
+    city = Column(String(30))
 
 
 class Purchase(Base):
     __tablename__ = 'purchase'
 
     id = Column(Integer, primary_key=True)
-    title = Column(String(40), nullable=False, server_default=text("''"))
-    brief = Column(String(100), nullable=False, server_default=text("''"))
-    navigation = Column(String(11), nullable=False, server_default=text("''"))
-    presell_count = Column(Integer, nullable=False)
-    sold_count = Column(Integer, nullable=False, server_default=text("'0'"))
-    satisfy_count = Column(Integer, nullable=False)
-    logo = Column(String(40), nullable=False, server_default=text("''"))
-    publisher_id = Column(Integer, nullable=False)
-    properties = Column(String(100), nullable=False, server_default=text("''"))
-    update_time = Column(DateTime, nullable=False)
-
-
-class Recommend(Base):
-    __tablename__ = 'recommend'
-
-    id = Column(Integer, primary_key=True)
-    seed_id = Column(Integer, nullable=False)
-    user_id = Column(Integer, nullable=False)
-    content = Column(Integer, nullable=False)
-    update_time = Column(DateTime, nullable=False)
+    name = Column(String(30))
+    price = Column(String(30))
+    publisher_id = Column(String(30))
+    create_time = Column(String(30))
+    update_time = Column(String(30))
+    start_time = Column(String(30))
+    end_time = Column(Integer)
 
 
 class User(Base):
@@ -97,7 +58,7 @@ class User(Base):
     province = Column(String(20), nullable=False, server_default=text("''"))
     city = Column(String(20), nullable=False, server_default=text("''"))
     country = Column(String(20), nullable=False, server_default=text("''"))
-    head_img_url = Column(String(150), nullable=False, server_default=text("''"))
+    profile = Column(String(150), nullable=False, server_default=text("''"))
     privilege = Column(String(40), nullable=False, server_default=text("''"))
     union_id = Column(String(30), nullable=False, server_default=text("''"))
     is_v = Column(Integer, nullable=False, server_default=text("'0'"))
@@ -106,7 +67,5 @@ class User(Base):
     company = Column(String(30))
     phone = Column(Integer)
     email = Column(String(30))
-    update_time = Column(DateTime)
-
-
-
+    update_time = Column(DateTime, nullable=False, server_default=text("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"))
+    create_time = Column(DateTime, nullable=False, server_default=text("'0000-00-00 00:00:00'"))
