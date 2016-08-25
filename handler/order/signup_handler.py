a06f2a5db53da9491e16c2708df8b13075aa482d
sunurdy <sunurdy@gmail.com>
2016-08-25 18:56:49 +0800

    订单增加purchase_id

--- handler/order/signup_handler.py
+++ handler/order/signup_handler.py
@@ -10,7 +10,7 @@ import json
 import tornado
 from datetime import datetime
 
-from model.models import db, Order, User, Purchase
+from model.models import db, Order, User, Purchase, Kind
 from handler.base.base_handler import BaseHandler
 
 
@@ -33,13 +33,18 @@ class SignupHandler(BaseHandler):
                 user.nickname = nickname
                 db.commit()
                 for i in kind_id:
-                    order = db.query(Order).filter_by(user_id=user.id, kind_id=i).first()
-                    if not order:
-                        new_order = Order(status=0, user_id=user.id, kind_id=i, create_time=datetime.now())
-                        new_order.save()
+                    kind = db.query(Kind).filter_by(id=kind_id).first()
+                    if kind:
+                        order = db.query(Order).filter_by(user_id=user.id, kind_id=i).first()
+                        if not order:
+                            new_order = Order(status=0, user_id=user.id, kind_id=i, purchase_id=kind.purchase_id, create_time=datetime.now())
+                            new_order.save()
+                    else:
+                        self.write({'reason': u'无此舞种', 'res': res})
                 self.write({'reason': '', 'res': res})
             else:
                 self.write({'reason': u'无此用户', 'res': res})
+
         else:
             # 返回数据，如果reason为空则表示成功，否则表示出错
             self.write({'reason': u'参数错误', 'res': res})
