8f7ab6065605bcc0a5b1bd3b64cda75dba731491
Guo Guangchuan <ggc0402@qq.com>
2016-09-08 19:37:42 +0800

    fix bug of send notify

--- handler/order/paynotify_handler.py
+++ handler/order/paynotify_handler.py
@@ -67,27 +67,30 @@ class PaynotifyHandler(BaseHandler):
             order.transaction_id = wechat_data.get('transaction_id', '')
             order.save()
         try:
-            template_res = {
-                'db': self.db,
-            }
-            if user_id and kind_ids and purchase_id:
-                user = db.query(User).filter_by(id=user_id).first()
-                template_res['keyword1'] = '%s(%s)' % (user.name, user.nickname)
-                template_res['open_id'] = user.open_id
-                purchase = db.query(Purchase).filter_by(id=purchase_id).first()
-                template_res['keyword3'] = purchase.begin_time.strftime('%Y-%m-%d %H:%M')
-                template_res['keyword4'] = purchase.place
-                kinds = []
-                price = 0
-                for kind_id in kind_ids:
-                    kind = db.query(Kind).filter_by(id=kind_id).first()
-                    kinds.append(kind.kind)
-                    price += kind.price
-                template_res['urls'] = '%s%s' % (utils.config.get('global', 'notify_detail_domain'), order.out_trade_no)
-                template_res['keyword2'] = '%s(%s)' % (purchase.body, ','.join(kinds))
-                template_res['first'] = '您已报名成功, 报名费共%s元' % (price * 0.01)
-                template_res['remark'] = '届时，我们期待您的参加！'
-                self.wechat.send_template(**template_res)
+            if wechat_data.get('is_subscribe', '') == 'Y':
+                template_res = {
+                    'db': self.db,
+                }
+                if user_id and kind_ids and purchase_id:
+                    user = db.query(User).filter_by(id=user_id).first()
+                    template_res['keyword1'] = '%s(%s)' % (user.name, user.nickname)
+                    template_res['open_id'] = user.open_id
+                    purchase = db.query(Purchase).filter_by(id=purchase_id).first()
+                    template_res['keyword3'] = purchase.begin_time.strftime('%Y-%m-%d %H:%M')
+                    template_res['keyword4'] = purchase.place
+                    kinds = []
+                    price = 0
+                    for kind_id in kind_ids:
+                        kind = db.query(Kind).filter_by(id=kind_id).first()
+                        kinds.append(kind.kind)
+                        price += kind.price
+                    template_res['urls'] = '%s%s' % (utils.config.get('global', 'notify_detail_domain'), order.out_trade_no)
+                    template_res['keyword2'] = '%s(%s)' % (purchase.body, ','.join(kinds))
+                    template_res['first'] = '您已报名成功, 报名费共%s元' % (price * 0.01)
+                    template_res['remark'] = '届时，我们期待您的参加！'
+                    self.wechat.send_template(**template_res)
+            else:
+                self.loger.error('%s   %s   %s' % (user_id, kind_ids, purchase_id))
         except Exception, e:
             self.loger.error(traceback.format_exc())
         self.write(wechat_server_notify.SUCCESS)
