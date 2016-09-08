986242469f41c5c1de9907afadf3b4fc3111b0f6
Guo Guangchuan <ggc0402@qq.com>
2016-09-09 01:01:05 +0800

    index jump

--- handler/purchase/index_handler.py
+++ handler/purchase/index_handler.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import urllib2
+import traceback
 from utils.code import *
 from utils.wechat import oauth
 from handler.base.base_handler import BaseHandler
@@ -11,9 +12,18 @@ __email__ = 'ggc0402@qq.com'
 class IndexHandler(BaseHandler):
     @oauth
     def get(self):
-        args = self.get_need_args('purchase_id')
-        self.session['index_url'] = self.request.uri
-        self.session.save()
-        purchase_id = urllib2.unquote(args['purchase_id'])
-        res = self.get_res(SUCCESS, res={"purchase_id": purchase_id, "finished": False})
-        self.render('purchase/index.html', **res)
+        try:
+            args = self.get_need_args('purchase_id', 'new')
+            self.session['index_url'] = self.request.uri
+            self.session.save()
+            new = args['new']
+            if not new:
+                new = False
+            else:
+                new = True
+            purchase_id = args['purchase_id']
+            res = self.get_res(SUCCESS, res={"purchase_id": purchase_id, "finished": False, "new": new})
+            self.loger.info(res)
+            self.render('purchase/index.html', **res)
+        except:
+            self.loger.info(traceback.format_exc())
